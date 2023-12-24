61f7466639cb0094e2ee0b98228e56bbe8ace386
Yusuke Watanabe <wyusuuke@gmail.com>
2023-12-25 07:35:36 +0900

    update

--- emacs-gpt.py
+++ emacs-gpt.py
@@ -4,25 +4,44 @@ import json
 import os
 import time
 
+ADDITIONAL_PROMPT = """
+Background: Now, I am using you as a debugging tool.
+Rule #1: When debug is required, please only return the revised script.
+Rule #2: Please highlight the issue fixing lines between the original and revised using "[REVISED #1]" tag as comments.
+Rule #3: When [REVISED #X] tags are already included, please increment them as [REVISED #X+1].
+Rule #4: When [REVISED #X] tags are removed, plese keep them as they are; you don't need to add the [REVISED #X] tags for them anymore.
+Rule #5: When no revision is added, please not return any code but just return "No revision needed."
+Rule #3: However, you shold avoid addiing unnecessary comments on the code; it's just durty.
+Rule #4: Your comments are not nesseccary as they are just distructive.
+Rule #5: When yes/no question is delivered, please respond it with the Point, Reason, and Example (PRE) method in about three sentences.
+"""
+
 
 def run_gpt(
-    api_key, engine, max_tokens, temperature, api_type, prompt_file, history_file
+    api_key,
+    engine,
+    max_tokens,
+    temperature,
+    api_type,
+    prompt_file,
+    history_file,
+    N_HISTORY=3,
 ):
     # Read the prompt
     with open(prompt_file, "r") as f:
         prompt = f.read()
 
+    # Prepend the ADDITIONAL_PROMPT to the user's prompt
+    prompt = ADDITIONAL_PROMPT + prompt
+
     # Load the history
     if api_type == "chat":
-        # Load the history if it exists, otherwise create an empty list
         if os.path.exists(history_file):
             with open(history_file, "r") as f:
                 history = json.load(f)
         else:
             history = []
-
         history.append({"role": "user", "content": prompt})
-
     else:
         history = None
 
@@ -35,21 +54,22 @@ def run_gpt(
             try:
                 response = openai.ChatCompletion.create(
                     model=engine,
-                    messages=history[-10:],  # Adjust this value as per your requirement
+                    messages=history[-N_HISTORY:],
                     max_tokens=int(max_tokens),
                     temperature=float(temperature),
                 )
-                # if successful, break the loop
                 break
             except openai.error.InvalidRequestError as e:
                 if "context length is" in str(e):
                     # Reduce the history and try again by excluding the first message
                     history = history[1:]
+                    print(e)
                 else:
                     raise e
 
             except openai.error.RateLimitError as e:
-                time.sleep(1)
+                print(e)
+                time.sleep(3)
 
         # Update the history with assistant's message
         history.append(
@@ -76,11 +96,13 @@ def run_gpt(
     for response in history:
         role = response["role"]
         role = {"user": "YOU", "assistant": "GPT"}[role]
-        content = response["content"]\
-            .replace("Assistant: ", "")\
-            .replace("assistant: ", "")\
-            .replace("User: ", "")\
+        content = (
+            response["content"]
+            .replace("Assistant: ", "")
+            .replace("assistant: ", "")
+            .replace("User: ", "")
             .replace("user: ", "")
+        )
         if role == "YOU":
             content.replace("\n\n", "")
 
@@ -88,28 +110,6 @@ def run_gpt(
         print()
         print(content)
         print("\n" + "=" * 60 + "\n")
-        
-    # for ii in range(len(history) // 2):
-    #     gpt_message = (
-    #         history[2 * ii + 1]["content"]
-    #         .replace("Assistant: ", "")
-    #         .replace("assistant: ", "")
-    #         # .replace("\n\n", "")
-    #     )
-
-    #     your_message = (
-    #         history[2 * ii]["content"]
-    #         .replace("User: ", "")
-    #         .replace("user: ", "")
-    #         .replace("\n\n", "")
-    #     )
-
-    #     if ii != 0:
-    #         print("YOU: " + your_message)
-    #         print("\n" + "=" * 60 + "\n")
-    #         # print()
-    #     print("GPT: " + gpt_message)
-    #     print("\n" + "=" * 60 + "\n")
 
 
 if __name__ == "__main__":
